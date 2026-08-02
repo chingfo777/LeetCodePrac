29cd476c413aaa67574d1c1475815f5690c563dd
Subha Shil <103221221+chingfo777@users.noreply.github.com>
2026-08-02 15:03:45 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (89.64%) - LeetSync

---
+++ 2107-find-unique-binary-string/find-unique-binary-string.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        res = []
+        for i in range(len(nums)):
+            # Invert the i-th character of the i-th string
+            res.append('1' if nums[i][i] == '0' else '0')
+            
+        return "".join(res)
