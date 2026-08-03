d3db69eead2f8c83741b3a3af0aa81632ba7c6da
Subha Shil <103221221+chingfo777@users.noreply.github.com>
2026-08-03 15:38:51 +0530

    Time: 515 ms (17.58%) | Memory: 22.3 MB (27.64%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.py
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.py
@@ -3,31 +3,30 @@ class Solution:
         n = len(s)
         s_double = s + s
         
-        # Construct the two possible target alternating strings of length 2*n
+        # Build target alternating strings of length 2*n
         alt1 = "".join("0" if i % 2 == 0 else "1" for i in range(2 * n))
         alt2 = "".join("1" if i % 2 == 0 else "0" for i in range(2 * n))
         
-        diff1 = 0
-        diff2 = 0
+        diff1, diff2 = 0, 0
         ans = float('inf')
-        
         left = 0
+        
         for right in range(2 * n):
-            # Track mismatches with the two alternating patterns
+            # Track mismatches with target patterns
             if s_double[right] != alt1[right]:
                 diff1 += 1
             if s_double[right] != alt2[right]:
                 diff2 += 1
-            
-            # Maintain a window of size n
+                
+            # Maintain a window of length n
             if right - left + 1 > n:
                 if s_double[left] != alt1[left]:
                     diff1 -= 1
                 if s_double[left] != alt2[left]:
                     diff2 -= 1
                 left += 1
-            
-            # When we have a valid window of size n, record the minimum flips
+                
+            # Update minimum flips when window size reaches n
             if right - left + 1 == n:
                 ans = min(ans, diff1, diff2)
                 
